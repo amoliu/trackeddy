ccaaeb32eb9867ffa8fc4a17e7943e7b48ee0538
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2017-08-02 16:47:32 +1000

    (V.a0.1)

--- trackeddy/physics.py
+++ trackeddy/physics.py
@@ -1,4 +1,6 @@
 import numpy as np
+import seawater as sw
+from sympy.physics.vector import curl
 
 def okuboweissparm(u,v,z):
     du_x=np.gradient(u[z,:,:],axis=1)
@@ -22,3 +24,13 @@ def geostrophicssh(eta,lat,lon):
 
 def EkE(eta,u,v):
     print 'Work in progress'
+    
+def PVort(S,T,P,U,V):
+    theta=np.zeros(np.shape(S))
+    for ii in range(0,np.shape(S)[1]):
+        for jj in range(0,np.shape(S)[2]):
+            theta[:,ii,jj]=sw.ptmp(S[:,ii,jj],T[:,ii,jj],P)
+    rho=sw.dens(S,T,P)
+    omega=7.2921159*10**(-5)
+    zeta=2*omega+curl(U,V)
+    Q=(1/rho)*(zeta)*np.gradient(theta)
